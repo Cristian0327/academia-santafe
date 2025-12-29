816c6f0f1363ceeb4139b2f1ef3314134e11ea34
Cristian <cristian032720@example.com>
2025-12-29 09:08:09 -0500

    Remove deprecated eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
+
 export default nextConfig;
